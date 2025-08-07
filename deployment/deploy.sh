77012a864caafe92d4642b9f6c549e49038fb791
Mateusz Papuga <mateuszpapuga24@gmail.com>
2025-08-07 12:05:13 +0200

    ⚡️ reduce downtime

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,5 +3,4 @@
 cp /root/pp/.env.travel-map-fe ./.env
 
 docker compose -f ./deployment/docker-compose.yml build
-docker compose -f ./deployment/docker-compose.yml stop
 docker compose -f ./deployment/docker-compose.yml up --detach
