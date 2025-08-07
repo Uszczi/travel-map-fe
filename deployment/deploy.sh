c1efdeee4838fe70ccad26d12c9b12e528665124
Mateusz Papuga <mateuszpapuga24@gmail.com>
2025-08-07 14:00:30 +0200

    🎨 unify workflow

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp /root/pp/.env.travel-map-fe ./.env
+DIRNAME=$(basename "$PWD")
+cp /root/pp/.env.$DIRNAME ./.env
 
 docker compose -f ./deployment/docker-compose.yml build
 docker compose -f ./deployment/docker-compose.yml up --detach
